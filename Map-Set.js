d4338709ac1e1d841267cb9eda5dca49fe93c013
MontaserMohamed <montasser.mohamed@fedis.com.eg>
2023-11-25 16:27:43 +0200

    add map-set and loops examples

---
+++ Map-Set.js
@@ -0,0 +1,38 @@
+// -------------------------------- Map --------------------------------
+// console.log('------------------------ Map --------------------------------------');
+
+
+// let myMap = new Map();
+
+// myMap.set("name", "John");
+// myMap.set(1, "One");
+// myMap.set({ key: "customObject" }, "Value for customObject");
+
+// // Accessing values using keys
+// console.log(myMap.get("name"));  // Output: John
+// console.log(myMap.get(1));        // Output: One
+
+// myMap.forEach((value, key) => {
+//     console.log(`${key}: ${value}`);
+// });
+
+
+
+// -------------------------------- Set --------------------------------
+// console.log('------------------------ Set --------------------------------------');
+
+
+// let mySet = new Set();
+
+// mySet.add(1);
+// mySet.add("Hello");
+// mySet.add({ key: "customObject" });
+// mySet.add(1); // Duplicate entry, will be ignored
+
+// // Checking if an element exists in the Set
+// console.log(mySet.has("Hello"));  // Output: true
+// console.log(mySet.has(2));        // Output: false
+
+// mySet.forEach((value) => {
+//     console.log(value);
+// });
