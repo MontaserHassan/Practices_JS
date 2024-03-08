63e06fa263509746b05aa1b2f31b349a807bbad2
MontaserHassan <montaser.hassan.bns@gmail.com>
2024-03-09 00:09:41 +0200

    commented in caching file

--- caching.js
+++ caching.js
@@ -38,62 +38,60 @@
 // ----------------------------------------------------------------------------------------
 
 
-const apiCache = new Map();
+// const apiCache = new Map();
 
-async function fetchFromApi(url) {
-    if (apiCache.has(url)) {
-        console.log(`Fetching ${url} response from cache`);
-        console.log("apiCache.has(url): ", apiCache.get(url));
-        return apiCache.get(url);
-    }
-    const response = await fetch(url);
-    const data = await response.json();
-    apiCache.set(url, data); // Store response in cache
-    return data;
-};
+// async function fetchFromApi(url) {
+//     if (apiCache.has(url)) {
+//         console.log(`Fetching ${url} response from cache`);
+//         console.log("apiCache.has(url): ", apiCache.get(url));
+//         return apiCache.get(url);
+//     }
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     apiCache.set(url, data); // Store response in cache
+//     return data;
+// };
 
-async function displayUserData(userId) {
-    const apiUrl = `https://jsonplaceholder.typicode.com/users/${userId}`;
+// async function displayUserData(userId) {
+//     const apiUrl = `https://jsonplaceholder.typicode.com/users/${userId}`;
 
-    const startTime = performance.now(); // Start 
+//     const startTime = performance.now(); // Start 
 
-    const userData = await fetchFromApi(apiUrl);
+//     const userData = await fetchFromApi(apiUrl);
 
-    const endTime = performance.now(); // end
-    const elapsedTime = endTime - startTime;
+//     const endTime = performance.now(); // end
+//     const elapsedTime = endTime - startTime;
 
-    console.log(`Time taken: ${elapsedTime.toFixed(2)} ms`);
-    console.log(userData);
-    console.log('//////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////////////////\n');
-};
-
-
-async function displayUsersData() {
-    const apiUrl = `https://jsonplaceholder.typicode.com/users`;
-
-    const startTime = performance.now(); // Start
-
-    const userData = await fetchFromApi(apiUrl);
-
-    const endTime = performance.now(); // end
-    const elapsedTime = endTime - startTime;
-
-    console.log(`Time taken: ${elapsedTime.toFixed(2)} ms`);
-    console.log(userData);
-    console.log('//////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////////////////\n');
-};
+//     console.log(`Time taken: ${elapsedTime.toFixed(2)} ms`);
+//     console.log(userData);
+//     console.log('//////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////////////////\n');
+// };
 
 
-displayUserData(1); // Fetches user data from API and caches it
-displayUserData(2); // Fetches user data from cache
-displayUserData(3); // Fetches user data from cache
+// async function displayUsersData() {
+//     const apiUrl = `https://jsonplaceholder.typicode.com/users`;
+
+//     const startTime = performance.now(); // Start
+
+//     const userData = await fetchFromApi(apiUrl);
+
+//     const endTime = performance.now(); // end
+//     const elapsedTime = endTime - startTime;
+
+//     console.log(`Time taken: ${elapsedTime.toFixed(2)} ms`);
+//     console.log(userData);
+//     console.log('//////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////////////////\n');
+// };
 
 
-displayUsersData();
+// displayUserData(1); // Fetches user data from API and caches it
+// displayUserData(2); // Fetches user data from cache
+// displayUserData(3); // Fetches user data from cache
 
-displayUsersData();
+
+// displayUsersData();
+
+// displayUsersData();
 
 
 // ----------------------------------------------------------------------------------------
-
-
